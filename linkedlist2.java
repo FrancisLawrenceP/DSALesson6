abaf2b9078a40d76f2e8f3a0cb04ba11dadcaf56
Adriaan Dimate <202310449@gordoncollege.edu.ph>
2024-10-29 02:51:48 +0000

    linkedlist2

---
+++ linkedlist2.java
@@ -0,0 +1,20 @@
+class linkedlist2
+{
+    Node head;
+
+    static class Node
+    {
+        int data;
+        Node next;
+        Node (int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    public static void main (String[] args)
+    {
+        linkedlist2 llist = new linkedlist2();
+    }
+}
